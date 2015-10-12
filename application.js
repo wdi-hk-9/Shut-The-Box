44b47780f98d8fa0f6369258bc50bb6631a5f1d2
Philippa Johnstone <pecj@hotmail.co.uk>
2015-10-12 18:08:56 +0800

    function working for roll dice, check open tiles against roll total

--- application.js
+++ application.js
@@ -1,6 +1,7 @@
 $(function(){
 
 var game = new Game();
+//var tile = $(".tile");
 
 rollDice();
 
@@ -10,41 +11,65 @@ function rollDice () {
   changeDiceImage(game.dice.dice1, $('#dice1') );
   changeDiceImage(game.dice.dice2, $('#dice2') );
   $('.roll-total').html("Roll Total: " + game.rollTotal());
+  checkOpenTiles();
+}
+
+function checkOpenTiles(){
+  var tile = $(".tile");
+  for (var i = 0; i < tile.length; i++){
+    //console.log(parseInt(tile.eq(i).html()));
+    game.tilesOpenTotal += parseInt(tile.eq(i).html());
+  }
+
+  if (game.rollTotal() > game.tilesOpenTotal) {
+    console.log("Game Over");
+  } else {
+    addClickEvent();
+  }
+}
+
+function addClickEvent(){
+  var tile = $(".tile");
+  for (var i = 0; i < tile.length; i++){
+    tile[i].addEventListener("click", closeTile);
+  }
+}
+
+function closeTile(e){
+  var selectedValue = parseInt($(e.target).val());
+  var index = game.tempPlayerSelected.indexOf(selectedValue);
+
+  if (index === -1) {
+    game.tempPlayerSelected.push(selectedValue);
+  } else {
+    game.tempPlayerSelected.splice(index, 1);
+  }
+  console.log("The array ", game.tempPlayerSelected);
+  game.playerTotal();
+  // console.log(game.tempPlayerSelected);
 }
 
 function changeDiceImage(dice, element){
   switch (dice) {
     case 1:
-      element.attr("src", "file:///var/folders/ct/7_7rql1n1wz2jz37vvqd0cpm0000gn/T/noun_dice_116708.png");
+      element.attr("src", "img/face1.png");
       break;
     case 2:
-      element.attr("src", "file:///var/folders/ct/7_7rql1n1wz2jz37vvqd0cpm0000gn/T/noun_dice_116707.png");
+      element.attr("src", "img/face2.png");
       break;
     case 3:
-      element.attr("src", "file:///var/folders/ct/7_7rql1n1wz2jz37vvqd0cpm0000gn/T/noun_dice_116713.png");
+      element.attr("src", "img/face3.png");
         break;
     case 4:
-      element.attr("src", "file:///var/folders/ct/7_7rql1n1wz2jz37vvqd0cpm0000gn/T/noun_dice_116712.png");
+      element.attr("src", "img/face4.png");
         break;
     case 5:
-      element.attr("src", "file:///var/folders/ct/7_7rql1n1wz2jz37vvqd0cpm0000gn/T/noun_dice_116709.png");
+      element.attr("src", "img/face5.png");
         break;
     case 6:
-      element.attr("src", "file:///var/folders/ct/7_7rql1n1wz2jz37vvqd0cpm0000gn/T/noun_dice_116710.png");
+      element.attr("src", "img/face6.png");
         break;
   }
 }
-
-var tilesOpen = $(".tile");
-for (var i = 0; i < tilesOpen.length; i++){
-  console.log(parseInt(tilesOpen.eq(i).html()));
-  game.tilesOpenTotal += parseInt(tilesOpen.eq(i).html());
-  //console.log(game.tilesOpenTotal);
-}
-
-
-// if game.tilesOpenTotal < rollTotal {
-//   alert("Game Over");
-// }
 
 });
--- game.js
+++ game.js
@@ -2,27 +2,34 @@
 
 var Game = function(){
   this.dice = null;
-  this.maxDice = 6;
   this.tilesOpenTotal = 0;
-
-
-
+  this.tempPlayerSelected = [];
+  this.tempPlayerTotal = 0;
 };
 
  // generate random number
-Game.prototype.randomDice = function(min,max){
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+Game.prototype.randomDice = function(){
+  return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
 };
 
 // assign random number to dices
 Game.prototype.diceFace = function(){
   this.dice = {
-    dice1: this.randomDice(1, this.maxDice),
-    dice2: this.randomDice(1, this.maxDice)
+    dice1: this.randomDice(),
+    dice2: this.randomDice()
   };
 };
 
 // retrieve sum of dices
 Game.prototype.rollTotal = function(){
   return (this.dice.dice1) + (this.dice.dice2);
+}
+
+Game.prototype.playerTotal = function(){
+  this.tempPlayerTotal = 0;
+  console.log("Current Total ", this.tempPlayerTotal)
+  for (var i = 0; i < this.tempPlayerSelected.length; i++) {
+    this.tempPlayerTotal += this.tempPlayerSelected[i];
+  }
+  console.log("After sum ", this.tempPlayerTotal);
 }
