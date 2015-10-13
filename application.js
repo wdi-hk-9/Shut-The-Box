947f4e1d9eeaade8d28fc9dd0823c1477a459ce4
Philippa Johnstone <pecj@hotmail.co.uk>
2015-10-13 14:47:20 +0800

    roll dice, disable tiles (if tile > dice roll), player select tile

--- application.js
+++ application.js
@@ -1,84 +1,69 @@
 $(function(){
-
-var game = new Game();
-
-$('.player-message').html("Player: Click Dice to Roll");
-
-$('div.dice').on('click', function rollDice () {
-  game.diceFace();
-  // console.log(game.rollTotal());
-  changeDiceImage(game.dice.dice1, $('#dice1') );
-  changeDiceImage(game.dice.dice2, $('#dice2') );
-  $('.roll-total').html("Roll Total: " + game.rollTotal());
-  checkOpenTiles();
-});
-
-function checkOpenTiles(){
+  // initial values
   var tile = $(".tile");
-  for (var i = 0; i < tile.length; i++){
-    console.log(parseInt(tile.eq(i).val()));
-    game.tilesOpenTotal += parseInt(tile.eq(i).val());
-  }
 
-  if (game.rollTotal() > game.tilesOpenTotal) {
-    console.log("Game Over");
-  } else {
-    addClickEvent();
-  }
-}
+  var updateMessage = function(playerMessage) {
+    $('.player-message').html(playerMessage);
 
-function addClickEvent(){
-  var tile = $(".tile");
-  for (var i = 0; i < tile.length; i++){
-    tile[i].addEventListener("click", closeTile);
-  }
-}
+    var msg = "Roll Total: " + game.dice.sum;
+    $('.roll-total').html(msg);
+  };
 
-function closeTile(e){
-  var value = $(e.target).val();
-  var selectedValue = parseInt($(e.target).html());
-  var index = game.tempPlayerSelected.indexOf(selectedValue);
-  if (index === -1) {
-    game.tempPlayerSelected.push(selectedValue);
-    ($(e.target).val(0).toggleClass("font-color"));
-  } else {
-    game.tempPlayerSelected.splice(index, 1);
-    ($(e.target).removeClass("font-color"));
+  // change dice image
+  var rollDice = function() {
+    game.roll();
+    $('#dice1').attr('src', 'img/face' + game.dice.dice1 +'.png');
+    $('#dice2').attr('src', 'img/face' + game.dice.dice2 +'.png');
+  };
+
+  // Smaller or equal the dice roll, AND not used
+  var titleisActive = function(tile) {
+    return (tile <= game.dice.sum);
+  };
+
+  // if tile > dice roll then disable tiles
+  var updateTiles = function() {
+    for(var tile = 1; tile < 10; tile ++) {
+      $tile = $('#p1-tile'+tile);
+
+      if (titleisActive(tile)) {
+        $tile.addClass('tile-active');
+        $tile.removeClass('tile-disabled');
+      } else {
+        $tile.addClass('tile-disabled');
+        $tile.removeClass('tile-active');
+      }
     }
-  //console.log("The array ", game.tempPlayerSelected);
-  game.playerTotal();
-  //console.log(game.tempPlayerSelected);
-  //console.log("After sum ", game.tempPlayerTotal);
-  checkTileDiceMatch();
-}
+  };
 
-function checkTileDiceMatch(){
-  if (game.rollTotal === game.tempPlayerTotal) {
-    console.log("roll total and tiles closed match!");
-  }
-}
+  //add or remove player selected tiles
+  $("a.tile").click(function( event ) {
+    var tileValue = parseInt($(this).html());
+    var index = game.tilesPlayerSelected.indexOf(tileValue);
 
-function changeDiceImage(dice, element){
-  switch (dice) {
-    case 1:
-      element.attr("src", "img/face1.png");
-      break;
-    case 2:
-      element.attr("src", "img/face2.png");
-      break;
-    case 3:
-      element.attr("src", "img/face3.png");
-        break;
-    case 4:
-      element.attr("src", "img/face4.png");
-        break;
-    case 5:
-      element.attr("src", "img/face5.png");
-        break;
-    case 6:
-      element.attr("src", "img/face6.png");
-        break;
+      if (index === -1) {
+        $(this).addClass('player-selected');
+        game.tilesPlayerSelected.push(tileValue);
+      } else {
+        game.tilesPlayerSelected.splice(index, 1);
+        $(this).removeClass('player-selected');
+      }
+  });
+
+  //check player selection with dice roll total and disable player selected tiles
+  var confirmPlayerTileSelection = function () {
+
   }
-}
+
+
+  $('#roll-btn').on('click', function () {
+
+    rollDice();
+    updateMessage();
+    updateTiles();
+
+  });
+
+  var game = new Game();
 
 });
--- game.js
+++ game.js
@@ -3,34 +3,37 @@
 var Game = function(){
   this.dice = null;
   this.tilesOpenTotal = 0;
-  this.tempPlayerSelected = [];
+  this.tilesPlayerSelected = [];
   this.tempPlayerTotal = 0;
-
 };
 
- // generate random number
-Game.prototype.randomDice = function(){
-  return Math.floor(Math.random() * (6 - 1 + 1)) + 1;
-};
-
-// assign random number to dices
-Game.prototype.diceFace = function(){
-  this.dice = {
-    dice1: this.randomDice(),
-    dice2: this.randomDice()
-  };
-};
-
-// retrieve sum of dices
-Game.prototype.rollTotal = function(){
-  return (this.dice.dice1) + (this.dice.dice2);
+var diceTileCombos = {
+  "2": [[2], [1,1]],
+  "3": [[3], [1,2]],
+  "4": [[4], [1,3]],
+  "5": [[5], [4,1], [2,3]],
+  "6": [[6], [1,5], [2,4], [3,2,1]],
+  "7": [[7], [6,1], [5,2], [4,3], [4,2,1]],
+  "8": [[8], [7,1], [6,2], [5,3], [3,4,1], [5,2,1]],
+  "9": [[9], [8,1], [7,2], [6,3], [5,4], [6,2,1], [5,3,1], [6,2,1], [4,3,2]],
+  "10": [[10], [9,1], [8,2], [7,3], [6,4], [7,2,1], [6,3,1], [5,4,1], [7,2,1], [5,3,2], [4,3,2,1]],
+  "11": [[11], [10,1], [9,2], [8,3], [7,4], [6,5], [8,2,1], [7,3,1], [6,4,1], [5,3,2,1], [6,3,2], [5,4,2], [5,3,1,2], [5,3,2,1]],
+  "12": [[12], [11,1], [10,2], [9,3], [8,4], [7,5], [9,2,1], [8,3,1], [7,4,1], [6,5,1], [6,3,2,1], [5,4,2,1], [9,2,1], [7,3,2] [6,4,2], [6,3,2,1], [5,4,3], [8,3,1], [7,3,2]]
 }
 
-Game.prototype.playerTotal = function(){
+// assign random number to dices
+Game.prototype.roll = function(){
+  this.dice = {
+    dice1: Math.floor(Math.random() * (6 - 1 + 1)) + 1,
+    dice2: Math.floor(Math.random() * (6 - 1 + 1)) + 1
+  };
+
+  this.dice.sum = this.dice.dice1 + this.dice.dice2;
+};
+
+Game.prototype.reCalculatePlayerTotal = function(){
   this.tempPlayerTotal = 0;
-  //console.log("Current Total ", this.tempPlayerTotal)
   for (var i = 0; i < this.tempPlayerSelected.length; i++) {
     this.tempPlayerTotal += this.tempPlayerSelected[i];
   }
-  //console.log("After sum ", this.tempPlayerTotal);
 }
